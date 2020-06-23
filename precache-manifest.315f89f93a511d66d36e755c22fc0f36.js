3728446c0332c90a1c8abb31bcf12bf37138d960
Nick Young <nick.young@auckland.ac.nz>
2020-06-24 11:03:50 +1200

    Updates

---
+++ precache-manifest.315f89f93a511d66d36e755c22fc0f36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fc0709a585cff57fd6a591d41261ea",
+    "url": "/TABA/index.html"
+  },
+  {
+    "revision": "a442f498150ddc87a0a9",
+    "url": "/TABA/static/css/main.9667f384.chunk.css"
+  },
+  {
+    "revision": "7554ca23a1e81dbd2bd4",
+    "url": "/TABA/static/js/2.15d56ca4.chunk.js"
+  },
+  {
+    "revision": "43d27a7ad473c842d3b1308068345458",
+    "url": "/TABA/static/js/2.15d56ca4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a442f498150ddc87a0a9",
+    "url": "/TABA/static/js/main.9d68dede.chunk.js"
+  },
+  {
+    "revision": "d5e9b8542fa4f9c02794",
+    "url": "/TABA/static/js/runtime-main.401a985d.js"
+  }
+]);
