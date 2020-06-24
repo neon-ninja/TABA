95df28a83705465802e306a0a55f2dabaaba18a2
Nick Young <nick.young@auckland.ac.nz>
2020-06-24 21:10:29 +1200

    NZ census

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 document.write(
-  `<h1 id="title">There and back again - a visualisation of the 2018 census commuter data</h1>
+  `<h1 id="title">There and back again - a visualisation of the 2018 NZ census commuter data</h1>
    <a href="https://github.com/neon-ninja/TABA">
     <img 
       style="position: absolute; top: 0; right: 0; border: 0;" 
