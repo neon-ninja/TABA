046bff6f8dcf196ffc466f3bbc67a8ebd07eb645
Nick Young <nick.young@auckland.ac.nz>
2020-06-24 21:08:40 +1200

    Add title

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 document.write(
-  `<a href="https://github.com/neon-ninja/TABA">
+  `<h1 id="title">There and back again - a visualisation of the 2018 census commuter data</h1>
+   <a href="https://github.com/neon-ninja/TABA">
     <img 
       style="position: absolute; top: 0; right: 0; border: 0;" 
       src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" 
